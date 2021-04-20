c65403d6bc1064b1367786e72202f8efdff9af24
0or7 <w.asubsubsub@gmail.com>
2021-04-20 14:39:54 +0900

    addM

---
+++ Hobby_1/Hobby_1_4_addM.php
@@ -0,0 +1,10 @@
+<?php
+//データベース定義
+        define('HOST', 'mysql1.php.xdomain.ne.jp');
+        define('USR', 'haveabook_user1');
+        define('PASS', 'waka7ari');
+        define('DB', 'haveabook_db');
+		
+class Hobby_1_4_addM{
+
+}
